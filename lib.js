8f4b46f38a57d7f9c4cce426758b2b3d5c0e0b5e
Mmitraa <117802325+Mmitraa@users.noreply.github.com>
2022-11-14 23:35:09 +0330

    Add files via upload

---
+++ lib.js
@@ -0,0 +1,68 @@
+console.log("Leave a secret message");
+alert('Hello techer');
+prompt('test1');
+var txt= document.getElementById ('goodwomen')
+txt.innerHTML= "man talash mikonam pas mitavanam .";
+var name1 = prompt("What is your name?");
+document.write(name1);
+var date= Date();
+var loction = window.location;
+document.write (loction+ "<br>")
+document.write(date + '<br>');
+var width = window.innerWidth;
+document.write (width + '<br>');
+var pi= 3.14;
+document.write (pi + '<br>');
+var status =false ;
+document.write(false +'<br>');
+var status =window.closed ;
+document.write(window.closed +'<br>');
+var x= 45+4;
+document.write(x +'<br>'   ) ;
+var x= 179-3;
+document.write(x +'<br>'   ) ;
+var x= 1045*4;
+document.write(x +'<br>'   ) ;
+var x= 45/4;
+document.write(x +'<br>'   ) ;
+var x= 45%4;
+document.write(x +'<br>'   ) ;
+var name1 = "mitra" +"     "+ " bita " 
+document.write (name1 +'<br>') ;
+var name1 = "mitra" +"     "+  400  
+document.write (name1 +'<br>') ;
+var txt=document.getElementById('paragrap');
+txt.innerHTML="mamnon ostad khob";
+var number2 = 7
+var number3 = 11
+document.write(number2<=number3   , "<br>");
+document.write(number2==number3  , '<br>');
+var number5=43;
+var number6=33;
+if(number5>=number6)
+{
+    alert("bozorg");
+} 
+var g = 77;
+var v= 44;
+document.write((v==22)||(g==77) +'<br>');
+var g = 2707;
+var v= 41;
+document.write((7454>v)&&(v>77) +'<br>');
+function sallam(){
+    alert("sallam ostad");
+}sallam();
+
+function class1 (web) {
+
+    document.write(web +'<br>')
+}
+temp="mitra +'<br>";
+class1(temp);
+function welcomeMsg(msg){
+    alert(msg);
+}
+var A ="hi mami";
+welcomeMsg (A);
+A="Goodbaye";
+welcomeMsg(A);
